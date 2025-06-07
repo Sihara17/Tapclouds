5033329ef0f7aea986df1e979949f27dd452f09d
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-06-07 22:09:56 +0700

    Add files via upload

---
+++ src/components/Navigation/index.tsx
@@ -0,0 +1,16 @@
+"use client"
+import { TabItem, Tabs } from "@worldcoin/mini-apps-ui-kit-react"
+import { Bank, Home, User } from "iconoir-react"
+import { useState } from "react"
+
+export const Navigation = () => {
+  const [value, setValue] = useState("home")
+
+  return (
+    <Tabs value={value} onValueChange={setValue}>
+      <TabItem value="home" icon={<Home />} label="Home" />
+      <TabItem value="wallet" icon={<Bank />} label="Leaderboard" />
+      <TabItem value="profile" icon={<User />} label="Profile" />
+    </Tabs>
+  )
+}
