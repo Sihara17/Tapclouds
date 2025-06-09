5437a5f8b9703a2d60a403a2d4eb2f6cb0111d7e
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-06-10 00:25:13 +0700

    Delete src directory

--- src/auth/wallet/client-helpers.ts
+++
@@ -1,7 +0,0 @@
-import crypto from "crypto"
-
-export const hashNonce = ({ nonce }: { nonce: string }) => {
-  const hmac = crypto.createHmac("sha256", process.env.HMAC_SECRET_KEY!)
-  hmac.update(nonce)
-  return hmac.digest("hex")
-}
--- src/lib/auth.ts
+++
@@ -1,3 +0,0 @@
-import { auth as nextAuth } from "@/auth"
-
-export const auth = nextAuth
